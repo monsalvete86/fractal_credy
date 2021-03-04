ff08fdd3793828e4c6c1a2c7cdbf58c61cdbad21
martin <marck123.mcdd@gmail.com>
2021-03-03 23:37:10 -0500

    estable 2

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Page;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class Pages extends Component
@@ -11,6 +12,55 @@ class Pages extends Component
     public $slug;
     public $title;
     public $content;
+
+    /**
+     * The validation rules
+     * 
+     * @return void
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'slug' => ['required', Rule::unique('pages', 'slug')],
+            'content' => 'required',
+        ];
+    }
+
+    /**
+     * Runs everytime the title
+     * variable is updated.
+     * 
+     * @param mixed $value
+     * @return void
+     */
+
+    public function updatedTitle($value)
+    {
+        $this->generateSlug($value);
+    }
+    /**
+     * The create function.
+     * 
+     * @return void
+     */
+    public function create()
+    {
+        $this->validate();
+        page::created($this->modelData());
+        $this->modalFormVisible = false;
+        $this->resetVars();
+    }
+
+    /**
+     * The read funtion.
+     * 
+     * @return void
+     */
+    public function read()
+    {
+        return Page::paginate(5);
+    }
 
     /**
      * shows the form modal
@@ -24,12 +74,57 @@ class Pages extends Component
     }
 
     /**
+     * The data for the model mapped
+     * in this component.
+     * 
+     * @return void
+     */
+    public function modelData()
+    {
+        return [
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'content' => $this->content,
+        ];
+    }
+
+    /**
+     * Resets all the variables
+     * to null.
+     * 
+     * @return void
+     */
+    public function resetVars()
+    {
+        $this->title = null;
+        $this->slug = null;
+        $this->content = null;
+    }
+
+    /**
+     * generate a url slug 
+     * base on the title. 
+     * 
+     * @param mixed $value
+     * @return void
+     */
+    private function generateSlug($value)
+    {
+        $process1 = str_replace('','-', $value);
+        $process2 = strtolower($process1);
+        $this->slug = $process2;
+
+    }
+
+    /**
      * the livewire render fuction.
      * 
      * @return void
      */
     public function render()
     {
-        return view('livewire.pages');
+        return view('livewire.pages', [
+            'data' => $this->read(),
+        ]);
     }
 }
--- resources/views/livewire/pages.blade.php
+++ resources/views/livewire/pages.blade.php
@@ -1,7 +1,44 @@
-<div class="p-6">
-    <x-jet-button wire:click="createShowModal">
-        {{ __('Create') }}
-    </x-jet-button>
+<div class="p-12">
+    <div class="flex items-center justify-end px-4 py-3 text-right sm:px-6">
+        <x-jet-button wire:click="createShowModal">
+            {{ __('Create') }}
+        </x-jet-button>
+    </div>
+
+    {{-- The data table --}}
+    <table class="min-w-full divide-y divide-black-200">
+        <thead>
+            <tr>
+                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-winder">Title</th>
+                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-winder">Link</th>
+                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-winder">Content</th>
+                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-winder">Actions</th>
+            </tr>
+        </thead>
+        <tbody class="bg-white divide-y divide-gray-200">
+            @if ($data->count())
+                @foreach ($data as $item)
+                    <tr>
+                        <td class="px-6 py-4 text-sm whitespace-no-wrap">{{ $item->title }}</td>
+                        <td class="px-6 py-4 text-sm whitespace-no-wrap">dummy link</td>
+                        <td class="px-6 py-4 text-sm whitespace-no-wrap">{!! $item-> content !!}</td>
+                        <td class="px-6 py-4 text-right text-sm"> 
+                            <x-jet-button wire:click="updateShowModal({{ $item->id }})">
+                                {{ __('Update') }}
+                            </x-jet-button>
+                            <x-jet-button wire:click="createShowModal">
+                                {{ __('Delet') }}
+                            </x-jet-button>    
+                        </td> 
+                    </tr>
+                @endforeach
+            @else 
+                    <tr>
+                        <td class="px-6 py-4 text-sm whitespace-no-wrap" colspan="4">No Results Found</td>
+                    </tr>
+            @endif
+        </tbody>
+    </table>
 
     {{-- Modal Form --}}
     <x-jet-dialog-modal wire:model="modalFormVisible">
@@ -10,7 +47,37 @@
         </x-slot>
 
         <x-slot name="content">
-            the form elements goes here
+            <div class="mt-4">
+                <x-jet-label for="title" value="{{ _('Title') }}" />
+                <x-jet-input id="title" class="block mt-1 w-full" type="title" wire:model.debounce.800ms="title" />
+                @error('title') <span class="error">{{ $message }}</span> @enderror
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="title" value="{{ _('Slug') }}" />
+                <div class="mt-1 flex rounded-md shadow-sm">
+                    <span class="inline-flex items-center px-3 rounded-1-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                        http://localhost:8000/
+                    </span>
+                    <input wire:model="slug" class="form-input flex-1 block w-full rounded-none rounded-r-md transition duration-150 ease-in-out sm:text-sm sm:leading-5" placeholder="url-slug">
+                </div> 
+                @error('slug') <span class="error">{{ $message }}</span> @enderror
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="title" value="{{ _('Content') }}" />
+                <div class="rounded-md shadow-sm">
+                    <div class="mt-1 bg-white">
+                        <div class="body-content" wire:ignore>
+                            <trix-editor
+                                class="trix-content"
+                                x-ref="trix"
+                                wire:model.debounce.100000ms="content"
+                                wire:key="trix-content-unique-key">
+                            </trix-editor>
+                        </div>
+                    </div>
+                </div>
+                @error('content') <span class="error">{{ $message }}</span> @enderror
+            </div>       
         </x-slot>
 
         <x-slot name="footer">
@@ -19,7 +86,7 @@
                 {{ __('Cancel') }}
             </x-jet-secondary-button>
 
-            <x-jet-danger-button wire:click="create" wire:loading.attr="disabled">
+            <x-jet-button wire:click="create" wire:loading.attr="disabled">
                 {{ __('Save') }}
             </x-jet-danger-button>
         </x-slot>
