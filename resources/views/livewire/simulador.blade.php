cdf87405b36d7d99332705ce7af0d109e605099c
isalazar00 <issalazar00@gmail.com>
2021-03-05 16:16:31 -0500

    Estilos

---
+++ resources/views/livewire/simulador.blade.php
@@ -0,0 +1,20 @@
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="h4 font-weight-bold">
+            {{ __('Simulador') }}
+        </h2>
+    </x-slot>
+    
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class=" overflow-hidden shadow-xl sm:rounded-lg">
+               
+               {{-- Because she competes with no one, no one can compete with her. --}}
+
+               <div class="card">
+               </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -16,6 +16,9 @@
                 </x-jet-nav-link>
                 <x-jet-nav-link href="{{ route('pages') }}" :active="request()->routeIs('pages')">
                     {{ __('Pages') }}
+                </x-jet-nav-link>|
+                <x-jet-nav-link href="{{ route('simulador') }}" :active="request()->routeIs('simulador')">
+                    {{ __('Simulador') }}
                 </x-jet-nav-link>
             </ul>
 
