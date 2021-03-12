60a03efbca16a07c273fab8cfee55ffaf690bf09
issalazar00 <is.salazar00@gmail.com>
2021-03-12 00:25:44 -0500

    Migracion tabla creditos

--- database/migrations/2021_03_06_005959_create_creditos_table.php
+++ database/migrations/2021_03_06_005959_create_creditos_table.php
@@ -23,6 +23,25 @@ class CreateCreditosTable extends Migration
             $table->decimal('tasa_interes', $precision = 8, $scale = 2);
             $table->timestamps();
         });
+
+        Schema::table('creditos', function (Blueprint $table) {
+            $table->date('fecha_inicio');
+            $table->boolean('deudor')->comment('Solo se confirma si tiene deudor');
+            $table->double('valor_cuota',12,4);
+            $table->double('valor_abonado',12,4);
+            $table->double('valor_capital',12,4);
+            $table->double('valor_interes',12,4);
+            $table->double('porcent_interes_anual',12,4);
+            $table->double('porcent_interes_mensual',12,4);
+            $table->tinyInteger('estado');
+            $table->integer('usu_crea');
+            $table->integer('id_sede');
+            $table->integer('dia_limite');
+            $table->renameColumn('nro_cuotas', 'cant_cuotas');
+            $table->renameColumn('nro_cuotas_pagas', 'cant_cuotas_pagadas');
+            $table->renameColumn('tasa_interes', 'interes_mensual');
+        });
+
     }
 
     /**
@@ -34,4 +53,15 @@ class CreateCreditosTable extends Migration
     {
         Schema::dropIfExists('creditos');
     }
+
+
+    /**
+    * Se pueden crear tablas temporales, que 
+    * son eliminadas cuando el usuario cierra sesión
+    *
+    * Schema::create('calculations', function (Blueprint $table) {
+    *   $table->temporary();    *
+    *   // ...
+    *});
+    **/
 }
