2c46d940d8dcdf2f34024a3fab25136d2bb82c84
martin <marck123.mcdd@gmail.com>
2021-03-26 23:28:45 -0500

    jetstream 10

--- resources/views/vendor/jetstream/components/dialog-modal.blade.php
+++ resources/views/vendor/jetstream/components/dialog-modal.blade.php
@@ -4,15 +4,15 @@
     <div class="modal-content">
         <div class="modal-header">
             <h5 class="modal-title"></h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">{{ $title }}
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>
         <div class="modal-body">
-            {{ $content }}
+           
         </div>
         <div class="modal-footer bg-light">
-            {{ $footer }}
+            {{ $footer}}
         </div>
     </div>
 </x-jet-modal>
