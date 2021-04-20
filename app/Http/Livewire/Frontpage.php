8b7d13b8f42c15e85377f6b294d500081c67069a
martin <marck123.mcdd@gmail.com>
2021-04-19 19:25:18 -0500

    pagos

--- app/Http/Livewire/Frontpage.php
+++ app/Http/Livewire/Frontpage.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Livewire;
 
-use App\Models\Page;
+use App\Models\Pago;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
@@ -9,83 +9,4 @@ class Frontpage extends Component
 {
    
 
-    /**
-     * The livewire mount function.
-     *
-     * @param  mixed $urlslug
-     * @return void
-     */
-    public function mount($urlslug = null)
-    {
-        $this->retrieveContent($urlslug);
-    }
-
-    /**
-     * Retrieves the content of the page.
-     *
-     * @param  mixed $urlslug
-     * @return void
-     */
-    public function retrieveContent($urlslug)
-    {
-        // Get home page if slug is empty
-        if (empty($urlslug)) {
-            $data = Page::where('is_default_home', true)->first();
-        } else {
-
-            // Get the page according to the slug value
-            $data = Page::where('slug', $urlslug)->first();
-
-            // If we can't retrieve anything, let's get the default 404 not found page
-            if (!$data) {
-                $data = Page::where('is_default_not_found', true)->first();
-            }
-        }
-
-        $this->title = $data->title;
-        $this->content = $data->content;
-        // $this->title ="title";
-        // $this->content ="content";
-    }
-
-    /**
-     * Gets all the sidebar links.
-     *
-     * @return void
-     */
-    private function sideBarLinks()
-    {
-        return DB::table('navigation_menus')
-            ->where('type', '=', 'SidebarNav')
-            ->orderBy('sequence', 'asc')
-            ->orderBy('created_at', 'asc')
-            ->get();
-    }
-
-    /**
-     * Get the top navigation links.
-     *
-     * @return void
-     */
-    private function topNavLinks()
-    {
-        return DB::table('navigation_menus')
-            ->where('type', '=', 'TopNav')
-            ->orderBy('sequence', 'asc')
-            ->orderBy('created_at', 'asc')
-            ->get();
-    }
-
-    /**
-     * The livewire render function.
-     *
-     * @return void
-     */
-    public function render()
-    {
-        return view('livewire.frontpage', [
-            'sideBarLinks' => $this->sideBarLinks(),
-            'topNavLinks' => $this->topNavLinks(),
-        ])->layout('layouts.frontpage');
-    }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
         <!-- Page Heading -->
         <header class="d-flex py-3 bg-white shadow-sm border-bottom">
             <div class="container">
-                {{ $header }}
+                
             </div>
         </header>
           
